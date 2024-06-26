bc75af76304587bf0a9b7b78b4e1b4ef769e26c4
shaktikadam1630 <shaktikadam512@gmail.com>
2024-06-26 16:51:42 +0530

    Initial commit

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+
+
+final baseUrl = 'http://192.168.43.13:3000/';
+final registrationUrl = baseUrl + "registration";
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/registration_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+         debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       primaryColor: Colors.black,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Registration(),
+    );
+  }
+}
+
